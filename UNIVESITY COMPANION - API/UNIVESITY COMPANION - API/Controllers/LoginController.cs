6a5c2f00312b08a0d210f747d6486a43927815ae
Flavius <flavius.soimosan@yahoo.com>
2022-12-07 18:36:10 +0200

    Configured Cors and made first call response

---
+++ UNIVESITY COMPANION - API/UNIVESITY COMPANION - API/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+    namespace UNIVESITY_COMPANION___API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController
+    {
+        [HttpGet]
+        public WeatherForecast Get()
+        {
+            var obj = new WeatherForecast();
+            obj.Value = "This is returned from api";
+            return obj;
+        }
+    }
+}
